0b301b46581817ac53b259330a9764a49629a6ff
dornascarol <carolinafdornas@gmail.com>
2024-06-09 22:02:02 -0300

    adiciona novas rotas echo

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -17,6 +18,10 @@ func main() {
 
 	// Routes
 	e.GET("/estudantes", getEstudantes)
+	e.POST("/estudantes", createEstudante)
+	e.GET("/estudantes/:id", getEstudante)
+	e.PUT("/estudantes/:id", updateEstudante)
+	e.DELETE("/estudantes/:id", deleteEstudante)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
@@ -25,4 +30,26 @@ func main() {
 // Handler
 func getEstudantes(c echo.Context) error {
 	return c.String(http.StatusOK, "Lista de todos os estudantes")
+}
+
+func createEstudante(c echo.Context) error {
+	return c.String(http.StatusOK, "Cadastro um estudante")
+}
+
+func getEstudante(c echo.Context) error {
+	id := c.Param("id")
+	getStud := fmt.Sprintf("Pega %s estudante", id)
+	return c.String(http.StatusOK, getStud)
+}
+
+func updateEstudante(c echo.Context) error {
+	id := c.Param("id")
+	updateStud := fmt.Sprintf("Atualiza %s estudante", id)
+	return c.String(http.StatusOK, updateStud)
+}
+
+func deleteEstudante(c echo.Context) error {
+	id := c.Param("id")
+	deleteStud := fmt.Sprintf("Deleta %s estudante", id)
+	return c.String(http.StatusOK, deleteStud)
 }
