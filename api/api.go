b56837001397bc2825a4bef08465cb05e44ae53f
dornascarol <carolinafdornas@gmail.com>
2024-06-26 22:35:57 -0300

    cria arquivo para os handlers

--- api/api.go
+++ api/api.go
@@ -1,9 +1,6 @@
 package api
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/dornascarol/API-estudantes/db"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -39,44 +36,4 @@ func (api *API) ConfigRotas() {
 
 func (api *API) ComecaServidor() error {
 	return api.Echo.Start(":8080")
-}
-
-func (api *API) getEstudantes(c echo.Context) error {
-	estudantes, err := api.DB.GetEstudantes()
-	if err != nil {
-		return c.String(http.StatusNotFound, "Falha em obter os estudantes")
-	}
-
-	return c.JSON(http.StatusOK, estudantes)
-}
-
-func (api *API) createEstudante(c echo.Context) error {
-	estudante := db.Estudante{}
-	if err := c.Bind(&estudante); err != nil {
-		return err
-	}
-
-	if err := api.DB.AddEstudante(estudante); err != nil {
-		return c.String(http.StatusInternalServerError, "Erro para cadastrar estudante")
-	}
-
-	return c.String(http.StatusOK, "Estudante cadastrado")
-}
-
-func (api *API) getEstudante(c echo.Context) error {
-	id := c.Param("id")
-	getStud := fmt.Sprintf("Pega %s estudante", id)
-	return c.String(http.StatusOK, getStud)
-}
-
-func (api *API) updateEstudante(c echo.Context) error {
-	id := c.Param("id")
-	updateStud := fmt.Sprintf("Atualiza %s estudante", id)
-	return c.String(http.StatusOK, updateStud)
-}
-
-func (api *API) deleteEstudante(c echo.Context) error {
-	id := c.Param("id")
-	deleteStud := fmt.Sprintf("Deleta %s estudante", id)
-	return c.String(http.StatusOK, deleteStud)
 }
